36be69125327de24090a3793e850165bf40539ea
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-06-17 00:20:53 +0530

    minor fix in themes

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Thahawuru Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Thahawuru/Thahawuru-docs',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Thahawuru - Sri Lanka`s Integrated Digital Identity Verification System.',
   },
 }
 
